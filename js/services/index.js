76a7aec964603a6105be280677e02385ddb86517
Emily Chao <elchao96@live.com>
2016-04-21 12:24:22 -0500

    added indexes, playing sounds

--- js/services/index.js
+++ js/services/index.js
@@ -1,1 +1,9 @@
 var indexServices = angular.module('index.services', []);
+
+indexServices.factory('Index', function($http) {
+    return {
+        getSample: function() {
+            return $http.get('./data/sample.json');
+        }
+    };
+});
