b6dddb2c88f24b3483b56c914d071bb735ea355c
Emily Chao <elchao96@live.com>
2016-04-23 14:46:21 -0500

    Revert "script to run sample portfolio only"

--- py/write_ticker_map.py
+++ py/write_ticker_map.py
@@ -8,7 +8,7 @@ with open('failed_tickers.txt', 'r') as error_file:
     lst2 = error_file.read().split(',')
     for i in range(len(lst2)):
         lst.append(lst2[i].strip())
-nasdaq_tickers = {}
+nasdaq_tickers = []
 with open('../data/nasdaq_list.csv', newline='') as csvfile:
     stockreader = csv.reader(csvfile, quotechar='|')
     i = 0
@@ -19,11 +19,11 @@ with open('../data/nasdaq_list.csv', newline='') as csvfile:
         ticker = row[0]
         name = row[1].strip('"')
         if ticker not in lst:
-            nasdaq_tickers[ticker] = name
+            nasdaq_tickers.append({ticker:name})
 print(nasdaq_tickers)
 with open('../data/nasdaq_ticker_map.json', 'w') as outfile:
     json.dump(nasdaq_tickers,outfile)
-nyse_tickers = {}
+nyse_tickers = []
 with open('../data/nyse_list.csv', newline='') as csvfile:
     stockreader = csv.reader(csvfile, quotechar='|')
     i = 0
@@ -34,26 +34,7 @@ with open('../data/nyse_list.csv', newline='') as csvfile:
         ticker = row[0]
         name = row[1].strip('"')
         if ticker not in lst:
-            nyse_tickers[ticker] = name
+            nyse_tickers.append({ticker:name})
 print(nyse_tickers)
 with open('../data/nyse_ticker_map.json', 'w') as outfile:
     json.dump(nyse_tickers,outfile)
-with open('../data/sample_portfolios.json', 'r') as sample_json:
-    sample_portfolios = json.load(sample_json)
-print(sample_portfolios.keys())
-
-sample_tickers = {}
-for company_type in sample_portfolios.keys():
-    print(sample_portfolios[company_type])
-    for ticker in sample_portfolios[company_type]:
-        if ticker in nyse_tickers:
-            sample_tickers[ticker] = nyse_tickers[ticker]
-            print(ticker, "nyse")
-        elif ticker in nasdaq_tickers:
-            sample_tickers[ticker] = nasdaq_tickers[ticker]
-            print(ticker, "nasdaq")
-        else:
-            print("Not found", ticker)
-with open('../data/sample_ticker_map.json', 'w') as outfile:
-    json.dump(sample_tickers, outfile)
-print(sample_tickers)
