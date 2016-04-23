da315f4b8b3801726658022c9dd3e7e5a89b4fdc
Emily Chao <elchao96@live.com>
2016-04-23 16:49:08 -0500

    adding these files for now

--- js/services/user.js
+++ js/services/user.js
@@ -1,6 +1,26 @@
 var userServices = angular.module('user.services', []);
 
-userServices.factory('Authorization', ['$firebaseAuth', 'FirebaseUrl', function($firebaseAuth, FirebaseUrl) {
-    var reference = new Firebase(FirebaseUrl);
+userServices.factory('Authorization', ['$firebaseAuth', function($firebaseAuth) {
+    var reference = new Firebase('https://cs467cdogs.firebaseio.com/');
     return $firebaseAuth(reference);
+}]);
+
+userServices.service('UserService', ['Authorization', function(Authorization) {
+    return {
+        signup: function(userData) {
+            return Authorization.$createUser({
+                email: userData.email,
+                password: userData.password
+            });
+        },
+        login: function(userData) {
+            return Authorization.$authWithPassword({
+                email: userData.email,
+                password: userData.password
+            });
+        },
+        logout: function() {
+            return Authorization.$unauth();
+        }
+    };
 }]);
