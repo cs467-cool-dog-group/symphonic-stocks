d2f01c1c131450fb3ce5e502a64b5eae27835fa8
Emily Chao <elchao96@live.com>
2016-04-23 14:49:34 -0500

    something

--- js/services/user.js
+++ js/services/user.js
@@ -1,1 +1,6 @@
 var userServices = angular.module('user.services', []);
+
+userServices.factory('Authorization', ['$firebaseAuth', 'FirebaseUrl', function($firebaseAuth, FirebaseUrl) {
+    var reference = new Firebase(FirebaseUrl);
+    return $firebaseAuth(reference);
+}]);
